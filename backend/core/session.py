745130e09d01e3d10a026974338934472f04fc29
Amogh Shukla <amoghshukla548@gmail.com>
2026-07-30 22:56:56 +0530

    feat: implement async database session management with SQLAlchemy

--- backend/core/session.py
+++ backend/core/session.py
@@ -1,1 +1,13 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+from backend.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL)
+
+async_session = async_sessionmaker(bind=engine,class_=AsyncSession)
+
+async def get_db():
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            session.close() 
