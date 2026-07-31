a70bac70ecd2c81e59a8b981e4f5d296f9807c00
Amogh Shukla <amoghshukla548@gmail.com>
2026-07-31 23:45:40 +0530

    feat: add user_name field to User model in SQLAlchemy

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,6 @@
 from uuid import UUID, uuid4
 
-from sqlalchemy import Column
+from sqlalchemy import Column, String
 from sqlalchemy.orm import Mapped, UUID as sa_UUID
 
 from backend.core.base import Base
@@ -8,4 +8,6 @@ from backend.core.base import Base
 class User(Base):
     __tablename__="user"
 
-    user_id : Mapped[UUID] = Column(sa_UUID, primary_key=True, index = True, nullable=False, default=uuid4()) 
+    user_id : Mapped[UUID] = Column(sa_UUID, primary_key=True, index = True, nullable=False, default=uuid4())
+    user_name : Mapped[str] = Column(String, nullable=False)
+     
