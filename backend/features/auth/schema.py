5efabf496b2830449dad8a3a1b0744950c5f5255
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-10 21:03:57 +0530

    feat: add SignupReponse class to schema.py for user response handling

--- backend/features/auth/schema.py
+++ backend/features/auth/schema.py
@@ -1,6 +1,14 @@
+from uuid import UUID
+
 from pydantic import BaseModel, EmailStr, Field
 
 class SignupRequest(BaseModel):
     user_name: str = Field(..., min_length=2, max_length=255)
     user_email: EmailStr
     user_password: str = Field(..., min_length=8)
+
+class SignupReponse(BaseModel):
+    user_id : UUID
+    user_name : str
+    role : str
+    user_email : str
