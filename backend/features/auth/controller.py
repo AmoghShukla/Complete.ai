a9d854a6bc3facf06f09a0406ad5423cdb2ebd36
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-12 21:45:22 +0530

    feat: implement signup function in authentication controller

--- backend/features/auth/controller.py
+++ backend/features/auth/controller.py
@@ -5,3 +5,5 @@ from .schema import SignupReponse, SignupRequest
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
+async def signup(data: SignupRequest, db: AsyncSession = Depends(get_db)):
+    
