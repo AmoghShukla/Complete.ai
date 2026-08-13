209ca41c43df36dece162739d968b13d3c204893
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-13 21:10:51 +0530

    fix: correct typo in login function dependency and add missing import

--- backend/features/auth/controller.py
+++ backend/features/auth/controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status, Depends
-from .schema import SignupReponse, SignupRequest
+from sqlalchemy.ext.asyncio import AsyncSession
+from .schema import SignupReponse, SignupRequest, LoginRequest
 
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
@@ -9,3 +10,4 @@ async def signup(data: SignupRequest, db: AsyncSession = Depends(get_db)):
     return await user_signup(data, db)
 
 @router.post(prefix='/login', status_code=status.HTTP_200_OK)
+async def login(data : LoginRequest, db : AsyncSession = Dependfs(get_db))
