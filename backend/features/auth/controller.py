32870adbbfb0d6573e40b1ffd1cc5c4520014051
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-13 21:11:22 +0530

    fix: correct typo in login function dependency and add missing return statement

--- backend/features/auth/controller.py
+++ backend/features/auth/controller.py
@@ -10,4 +10,5 @@ async def signup(data: SignupRequest, db: AsyncSession = Depends(get_db)):
     return await user_signup(data, db)
 
 @router.post(prefix='/login', status_code=status.HTTP_200_OK)
-async def login(data : LoginRequest, db : AsyncSession = Dependfs(get_db))
+async def login(data : LoginRequest, db : AsyncSession = Depends(get_db)):
+    return await user_login(data, db)
