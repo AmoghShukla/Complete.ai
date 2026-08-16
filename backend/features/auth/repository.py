66519dc1144c7eb23ffd33fc83c1c6dc2abf3beb
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-16 19:19:33 +0530

    feat: add error handling to async signup method in UserRepository

--- backend/features/auth/repository.py
+++ backend/features/auth/repository.py
@@ -1,9 +1,15 @@
-
+from sqlalchemy.exc import SQLAlchemyError
 
 class UserRepository:
 
     async def signup(data, db):
-        db.add(data)
-        await db.flush()
-        await db.refresh(data)
-        return data
+        try:
+            db.add(data)
+            await db.flush()
+            await db.refresh(data)
+            return data
+        except SQLAlchemyError as e:
+            await db.rollback()
+            raise DatabaseError()
+
+
