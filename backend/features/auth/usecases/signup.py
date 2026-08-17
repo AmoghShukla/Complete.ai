f98c668511c2d1be4ec07e291fbaa90a79cba192
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-17 22:39:31 +0530

    Addded User Email Check

--- backend/features/auth/usecases/signup.py
+++ backend/features/auth/usecases/signup.py
@@ -1,7 +1,10 @@
-
-
 from sqlalchemy.ext.asyncio import AsyncSession
+from backend.features.auth.repository import UserRepository
 from backend.features.auth.schema import SignupRequest
 
 
 async def signup(data : SignupRequest, db :AsyncSession):
+    '''check whether the enetered email is unique or not'''
+    user_exists = await UserRepository.get_user_by_email(data.user_email, db)
+    if user_exists:
+        raise ConflictException('Email Already Registered')
