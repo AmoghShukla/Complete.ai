4f79494843efa1632da33706db2d053579b2eb20
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-18 22:54:15 +0530

    feat: add password verification method to Security class with error handling

--- backend/utilities/security.py
+++ backend/utilities/security.py
@@ -14,3 +14,10 @@ class Security:
             password_context.hash(password)
         except Exception as e:
             raise BadRequestException("Error while hashing the password")
+
+    @staticmethod
+    def verify_password(plain_password, hashed_password):
+        try:
+            password_context.verify(plain_password, hashed_password)
+        except Exception as e:
+            raise BadRequestException("Error while Verifying the Password!!")
