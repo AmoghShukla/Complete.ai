f2de1092aa0abe4a4b5078e7caf0b369706f50de
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-18 22:51:01 +0530

    feat: implement password hashing method in Security class with error handling

--- backend/utilities/security.py
+++ backend/utilities/security.py
@@ -1,5 +1,7 @@
 from pwdlib import PasswordHash
 
+from backend.utilities.exceptions import BadRequestException
+
 
 
 password_context : PasswordHash = PasswordHash.recommended()
@@ -7,4 +9,8 @@ password_context : PasswordHash = PasswordHash.recommended()
 class Security:
 
     @staticmethod
-    def hash_password() 
+    def hash_password(password : str):
+        try:
+            password_context.hash(password)
+        except Exception as e:
+            raise BadRequestException("Error while hashing the password")
