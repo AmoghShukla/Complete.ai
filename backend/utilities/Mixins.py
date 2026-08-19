d8acabe876edbc6197a950439037e3591be62edc
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-19 20:07:09 +0530

    feat: add AuditTrailMixin for tracking creation and update timestamps

---
+++ backend/utilities/Mixins.py
@@ -0,0 +1,15 @@
+
+
+
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class AuditTrailMixin:
+
+    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    updated_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    is_deleted : Mapped[bool] = mapped_column(Boolean, default=False)
+
