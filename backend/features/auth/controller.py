1f15f00a823c1a501e24c3e9f73ced22e721bf83
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-24 13:19:07 +0530

    fix: correct function call in signup endpoint to use the proper signup function

--- backend/features/auth/controller.py
+++ backend/features/auth/controller.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from backend.core.session import get_db
+
 from .schema import SignupReponse, SignupRequest, LoginRequest
 from .usecases.login import user_login
 from .usecases.signup import signup
@@ -9,7 +11,7 @@ router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def signup(data: SignupRequest, db: AsyncSession = Depends(get_db)):
-    return await user_signup(data, db)
+    return await signup(data, db)
 
 @router.post(prefix='/login', status_code=status.HTTP_200_OK)
 async def login(data : LoginRequest, db : AsyncSession = Depends(get_db)):
