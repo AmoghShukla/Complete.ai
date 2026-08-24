1d7fcbd22d6fdb9ecab880738e790c82b719100a
Amogh Shukla <amoghshukla548@gmail.com>
2026-08-24 13:35:16 +0530

    fix: implement Settings class for database configuration and initialize FastAPI application

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,4 +1,10 @@
-class Settings:
-    pass
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DB_PORT : int
+    DB_USER : str
+    DB_NAME : str
+    DB_PASSWORD : str
+    DB_HOST : str
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from backend.features.auth.controller import router as AuthRouter
+
+app = FastAPI(
+    title = "Complete.AI",
+    version = "1.0.0"    
+    )
+
+app.include_router(AuthRouter)
+
+@app.get('/', tags=['Health'])
+def health():
+    return JSONResponse(
+        content={
+            "message" : "Congratulations, Your Application is up and running!!"
+        }
+    )
