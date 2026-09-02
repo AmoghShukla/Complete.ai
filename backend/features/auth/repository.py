078cfd379b8d09a0c0ac8f3ccadbcb500a6c557f
Amogh Shukla <amoghshukla548@gmail.com>
2026-09-02 22:38:50 +0530

    fix: correct indentation in get_user_by_email method

--- backend/features/auth/repository.py
+++ backend/features/auth/repository.py
@@ -21,7 +21,7 @@ class UserRepository:
     async def get_user_by_email(email_id: str, db: AsyncSession) -> User | None:
         try:
             result = await db.execute(
-                            select(User).where(
+                        select(User).where(
                                 User.user_email == email_id,
                                 User.is_deleted == False
                             )
