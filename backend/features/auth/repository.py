b8a1b0d0cef0762b259cf65227fb3a4a8ad32633
Amogh Shukla <amoghshukla548@gmail.com>
2026-09-03 22:26:57 +0530

    fix: improve error handling in get_user_by_email method

--- backend/features/auth/repository.py
+++ backend/features/auth/repository.py
@@ -28,6 +28,6 @@ class UserRepository:
                         )
             return result.scalars().first()
         except SQLAlchemyError as e:
-            raise 
+            raise DatabaseError() from e
 
 
