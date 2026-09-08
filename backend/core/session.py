66061286d92ca49a2a023684e4b01c035db4bad4
Amogh Shukla <amoghshukla548@gmail.com>
2026-09-08 22:25:34 +0530

    fix: add whitespace for improved readability in get_db function

--- backend/core/session.py
+++ backend/core/session.py
@@ -6,6 +6,7 @@ engine = create_async_engine(settings.DATABASE_URL)
 async_session = async_sessionmaker(bind=engine,class_=AsyncSession)
 
 async def get_db():
+    
     async with async_session() as session:
         try:
             yield session
