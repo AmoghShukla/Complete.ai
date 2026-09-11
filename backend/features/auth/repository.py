d599ea0c810cda73512164aff0839a07e0725d36
Amogh Shukla <amoghshukla548@gmail.com>
2026-09-11 21:38:07 +0530

    fix(auth): remove unnecessary blank line in get_user_by_email method

--- backend/features/auth/repository.py
+++ backend/features/auth/repository.py
@@ -20,7 +20,6 @@ class UserRepository:
     @staticmethod
     async def get_user_by_email(email_id: str, db: AsyncSession) -> User | None:
         try:
-            
             result = await db.execute(
                         select(User).where(
                                 User.user_email == email_id,
